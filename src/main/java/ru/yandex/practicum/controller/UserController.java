575b4aea08de9dd445ff10bc657317b1fb977c53
Данни Титуанья <davila.tituanya@gazprombank.ru>
2025-04-07 23:20:52 +0300

    Add initial files

---
+++ src/main/java/ru/yandex/practicum/controller/UserController.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users") // Контроллер обрабатывает запросы /users
+public class UserController {
+
+    @GetMapping // GET-запрос /users
+    public String users() {
+        return "users"; // Возвращаем название шаблона — users.html
+    }
+
+}
