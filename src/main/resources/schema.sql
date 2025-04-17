1eab83617bb4eea2870502312befaba5c81f8b6f
Данни Титуанья <davila.tituanya@gazprombank.ru>
2025-04-17 12:15:02 +0300

    Fix schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,21 @@
 CREATE TABLE IF NOT EXISTS posts (
-        id BIGSERIAL PRIMARY KEY,
-        title VARCHAR(256) NOT NULL,
-        text TEXT NOT NULL,
-        short_description VARCHAR(256) NOT NULL,
-        image_path VARCHAR(512),
-        likes INTEGER DEFAULT 0
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(256) NOT NULL,
+    text TEXT NOT NULL,
+    short_description VARCHAR(256) NOT NULL,
+    image_path VARCHAR(512),
+    likes INTEGER DEFAULT 0
     );
 
-INSERT INTO posts("Vacations", "This is a post about wonderful vacations, when are u going on vacations", "vacations of my dreams",
-                  "images/default.jpg", 0);
-INSERT INTO posts("Animals", "This is a post about animals, about pets and everythin about animals", "vacations of my dreams",
-                  "images/default.jpg", 0);
-INSERT INTO posts("Hobbies", "This is a post about wonderful hobbies", "vacations of my dreams",
-                  "images/default.jpg", 0);
+
+INSERT INTO posts(title, text, short_description, image_path, likes)
+VALUES ('Vacations', 'This is a post about wonderful vacations, when are u going on vacations', 'vacations of my dreams',
+        'images/default.jpg', 0);
+
+INSERT INTO posts(title, text, short_description, image_path, likes)
+VALUES ('Animals', 'This is a post about animals, about pets and everything about animals', 'animals and pets',
+        'images/default.jpg', 0);
+
+INSERT INTO posts(title, text, short_description, image_path, likes)
+VALUES ('Hobbies', 'This is a post about wonderful hobbies', 'my favorite hobbies',
+        'images/default.jpg', 0);
--- target/myapp-web/WEB-INF/classes/schema.sql
+++ target/myapp-web/WEB-INF/classes/schema.sql
@@ -7,9 +7,12 @@ CREATE TABLE IF NOT EXISTS posts (
         likes INTEGER DEFAULT 0
     );
 
-INSERT INTO posts("Vacations", "This is a post about wonderful vacations, when are u going on vacations", "vacations of my dreams",
+INSERT INTO posts(title, text, short_description, image_path, likes) values ("Vacations", "This is a post about wonderful vacations, when are u going on vacations", "vacations of my dreams",
                   "images/default.jpg", 0);
-INSERT INTO posts("Animals", "This is a post about animals, about pets and everythin about animals", "vacations of my dreams",
+INSERT INTO posts(title, text, short_description, image_path, likes) values ("Animals", "This is a post about animals, about pets and everythin about animals", "vacations of my dreams",
                   "images/default.jpg", 0);
-INSERT INTO posts("Hobbies", "This is a post about wonderful hobbies", "vacations of my dreams",
+INSERT INTO posts(title, text, short_description, image_path, likes) values ("Hobbies", "This is a post about wonderful hobbies", "vacations of my dreams",
                   "images/default.jpg", 0);
+-- insert into users(first_name, last_name, age, active) values ('Иван', 'Иванов', 30, true);
+-- insert into users(first_name, last_name, age, active) values ('Пётр', 'Петров', 25, false);
+-- insert into users(first_name, last_name, age, active) values ('Мария', 'Сидорова', 28, true);
