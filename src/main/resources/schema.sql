efe7775501f421c8a55d12c0fff6bb270605d835
Данни Титуанья <davila.tituanya@gazprombank.ru>
2025-04-17 11:53:18 +0300

    Update schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,15 @@
--- Таблица с пользователями
-create table if not exists users(
-                                    id bigserial primary key,
-                                    first_name varchar(256) not null,
-    last_name varchar(256) not null,
-    age integer not null,
-    active boolean not null);
+CREATE TABLE IF NOT EXISTS posts (
+        id BIGSERIAL PRIMARY KEY,
+        title VARCHAR(256) NOT NULL,
+        text TEXT NOT NULL,
+        short_description VARCHAR(256) NOT NULL,
+        image_path VARCHAR(512),
+        likes INTEGER DEFAULT 0
+    );
 
-insert into users(first_name, last_name, age, active) values ('Иван', 'Иванов', 30, true);
-insert into users(first_name, last_name, age, active) values ('Пётр', 'Петров', 25, false);
-insert into users(first_name, last_name, age, active) values ('Мария', 'Сидорова', 28, true);
+INSERT INTO posts("Vacations", "This is a post about wonderful vacations, when are u going on vacations", "vacations of my dreams",
+                  "images/default.jpg", 0);
+INSERT INTO posts("Animals", "This is a post about animals, about pets and everythin about animals", "vacations of my dreams",
+                  "images/default.jpg", 0);
+INSERT INTO posts("Hobbies", "This is a post about wonderful hobbies", "vacations of my dreams",
+                  "images/default.jpg", 0);
