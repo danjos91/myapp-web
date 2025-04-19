cf92a30804c6cd6e990c453fa20df23c37f2e37f
Данни Титуанья <davila.tituanya@gazprombank.ru>
2025-04-19 10:36:07 +0300

    Update test scheme

--- src/test/resources/test-schema.sql
+++ src/test/resources/test-schema.sql
@@ -1,9 +1,19 @@
--- Таблица с пользователями
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-                       id BIGINT PRIMARY KEY,
-                       first_name VARCHAR(255),
-                       last_name VARCHAR(255),
-                       age INT,
-                       active BOOLEAN
-);
+
+DROP TABLE IF EXISTS posts;
+CREATE TABLE IF NOT EXISTS posts (
+                                     id BIGSERIAL PRIMARY KEY,
+                                     title VARCHAR(256) NOT NULL,
+                                     text TEXT NOT NULL,
+                                     image_path VARCHAR(512),
+                                     tags TEXT,
+                                     likes INTEGER DEFAULT 0
+    );
+
+
+DROP TABLE IF EXISTS comments;
+CREATE TABLE IF NOT EXISTS comments (
+                                        id BIGSERIAL PRIMARY KEY,
+                                        post_id BIGINT,
+                                        text TEXT NOT NULL,
+                                        FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+    );
