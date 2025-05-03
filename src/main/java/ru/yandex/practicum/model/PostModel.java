efc380e214af267b3122562b7a4d575e86667d94
Danny Tituaña <danny_jtd@hotmail.com>
2025-05-03 15:32:06 +0300

    Add test files

--- src/main/java/ru/yandex/practicum/model/PostModel.java
+++ src/main/java/ru/yandex/practicum/model/PostModel.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.model;
 import lombok.Data;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 
 @Data
 public class PostModel {
